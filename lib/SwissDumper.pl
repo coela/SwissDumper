05813f5ad9c75771ba1c74f74f908f30fac3caa9
coela <coela.st@gmail.com>
2012-05-15 14:29:11 +0900

    added subs

--- lib/SwissDumper.pl
+++ lib/SwissDumper.pl
@@ -6,30 +6,65 @@ use TokyoCabinet;
 use Storable qw(nstore store_fd nstore_fd freeze thaw dclone);
 
 my $tcname = 'uniprot.tch';
+my $acname = 'uniprot_ac.tch';
 
-# create the object
-my $hdb = TokyoCabinet::HDB->new();
+#store_ac_id($ARGV[0], $acname);
+#store_swissknife();
 
-# open the database
-if (!$hdb->open($tcname, $hdb->OWRITER | $hdb->OCREAT)) {
-	my $ecode = $hdb->ecode();
-	printf STDERR ("open error: %s\n", $hdb->errmsg($ecode));
-}
+sub store_ac_id {
+	my $fh = shift // die;
 
-$/ = "\/\/\n";
+	my $tcname = shift // die;
+	my $hdb = TokyoCabinet::HDB->new();
 
-$ARGV[0] or die;
-open my $UP, '<', $ARGV[0] or die $!;
-while (<$UP>) {
-	my $entry = SWISS::Entry->fromText($_);	
-	if (! $hdb->put($entry->ID, freeze $entry)){
+	if (!$hdb->open($tcname, $hdb->OWRITER | $hdb->OCREAT)) {
 		my $ecode = $hdb->ecode();
-		printf STDERR ("put error: %s\n", $hdb->errmsg($ecode) );
+		printf STDERR ("open error: %s\n", $hdb->errmsg($ecode));
+	}
+
+	open my $UP, '<', $fh or die $!;
+	$/ = "\/\/\n";
+	while (<$UP>) {
+		my $entry = SWISS::Entry->fromText($_);
+		for my $ac (@{$entry->ACs->list}){
+			if (! $hdb->put($ac, $entry->ID)){
+				my $ecode = $hdb->ecode();
+				printf STDERR ("put error: %s\n", $hdb->errmsg($ecode) );
+			}
+		}
+	}
+
+	if (!$hdb->close()) {
+		my $ecode = $hdb->ecode();
+		printf STDERR ("close error: %s\n", $hdb->errmsg($ecode));
 	}
 }
 
-if (!$hdb->close()) {
-	my $ecode = $hdb->ecode();
-	printf STDERR ("close error: %s\n", $hdb->errmsg($ecode));
-}
+sub store_swissknife {
+	my $fh = shift // die;
+	my $tcname = shift // die;
 
+	my $hdb = TokyoCabinet::HDB->new();
+
+	if (!$hdb->open($tcname, $hdb->OWRITER | $hdb->OCREAT)) {
+		my $ecode = $hdb->ecode();
+		printf STDERR ("open error: %s\n", $hdb->errmsg($ecode));
+	}
+
+
+	open my $UP, '<', $fh or die $!;
+
+	$/ = "\/\/\n";
+	while (<$UP>) {
+		my $entry = SWISS::Entry->fromText($_);
+		if (! $hdb->put($entry->ID, freeze($entry))){
+			my $ecode = $hdb->ecode();
+			printf STDERR ("put error: %s\n", $hdb->errmsg($ecode) );
+		}
+	}
+
+	if (!$hdb->close()) {
+		my $ecode = $hdb->ecode();
+		printf STDERR ("close error: %s\n", $hdb->errmsg($ecode));
+	}
+}
